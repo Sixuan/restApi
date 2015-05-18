2de25daacd9ada2ecdc1ae9a88662b3912fa06a5
Sixuan Liu <sixliu0913@gmail.com>
2015-05-17 21:37:58 -0400

    info model

---
+++ app/models/InfoModel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sixuan
+ * Date: 5/17/15
+ * Time: 4:32 PM
+ */
+
+namespace app\models;
+
+
+class InfoModel extends BaseModel{
+
+    public function insertCountHeatmap($data) {
+        print_r($data);
+        return true;
+    }
+
+}
